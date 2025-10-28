34f5c8759f442f84ba65f33824e1d3e0a5e2948e
André <=>
2025-10-28 09:16:20 -0300

    Adicionando componente de login com React

---
+++ front-fabrica/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Bem-vindo ao Café au Caramelo ☕</h1>
+    </div>
+  );
+}
+
+export default Home;
